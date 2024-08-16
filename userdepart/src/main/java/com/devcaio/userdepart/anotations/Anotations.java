bc05ccf5d57e243cadb635defb23d6bd90a34c0e
CaioGomes18 <caiomarcelinog@gmail.com>
2024-08-16 00:07:47 -0300

    Fisrt commit of the project

---
+++ userdepart/src/main/java/com/devcaio/userdepart/anotations/Anotations.java
@@ -0,0 +1,8 @@
+package com.devcaio.userdepart.anotations;
+
+public class Anotations {
+	/*Geralmente na utilização de um banco de dados temos que fazer a conversão de dados e para isso
+	 * utilizamos uma ferramenta de orm que transporta os dados da linguagem para alinguagem relacional que
+	 * é a linguagem do banco de dados o nome ORM significa Mapeamento Objeto Relacional
+	 */
+}
---
+++ userdepart/src/main/java/com/devcaio/userdepart/domain/User.java
@@ -0,0 +1,51 @@
+package com.devcaio.userdepart.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idUser;
+	private String nomeUser;
+	private String emailUser;
+	
+	
+	//esse atributo faz uma conexão ao departamento que o usuário pertence
+	@ManyToOne
+	@JoinColumn(name = "department_id")
+	private Department departamentoUser;
+	
+	public User() {}
+	
+	public long getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(long idUser) {
+		this.idUser = idUser;
+	}
+	public String getNomeUser() {
+		return nomeUser;
+	}
+	public void setNomeUser(String nomeUser) {
+		this.nomeUser = nomeUser;
+	}
+	public String getEmailUser() {
+		return emailUser;
+	}
+	public void setEmailUser(String emailUser) {
+		this.emailUser = emailUser;
+	}
+	
+	
+	
+}
